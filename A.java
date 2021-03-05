e8330f2b25263abc44ed964e7268b4d73721980c
Andrey <stantonfeng@yandex.ru>
2021-03-05 17:09:10 +0300

    Я просто пробую

---
+++ A.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class A {
+    void doSomething() {
+        System.out.println(5);
+    }
+}
---
+++ B.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class B extends A {
+    void doSomething() {
+        System.out.println(10);
+    }
+}
